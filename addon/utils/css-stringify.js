93aea9515778b446ac9f1a6785eba0ab0ef908c2
Ryan Nosek <rnosek@rnmbp.attlocal.net>
2022-07-19 19:41:18 -0700

    Fixing htmlSafe import location

--- addon/utils/css-stringify.js
+++ addon/utils/css-stringify.js
@@ -1,5 +1,5 @@
 import { isEmpty } from '@ember/utils';
-import { dasherize, htmlSafe } from '@ember/string';
+import { dasherize, htmlSafe } from '@ember/template';
 
 const PREFIXES = ['webkit'];
 const PREFIXED_PROPS = [
